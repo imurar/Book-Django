6c28027ca1948a50b8d8e2b7ba8da196af3558a6
imurar <imurar@outlook.com>
2025-06-20 19:55:00 +0900

    PostgreSQL対応のためにDjangoのDB設定を更新

--- backend/Django/settings.py
+++ backend/Django/settings.py
@@ -81,10 +81,15 @@ WSGI_APPLICATION = 'Django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
