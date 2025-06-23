b0ef4a2c96072f8dc5bd2739a3fe01f677d68dc1
imurar <imurar@outlook.com>
2025-06-23 09:01:39 +0900

    Vueで一覧の初期ページを作成

---
+++ frontend/src/components/BookList.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { onMounted, ref } from "vue";
+import { getBooks } from "../api/books";
+
+const books = ref([]);
+
+onMounted(async () => {
+  books.value = await getBooks();
+});
+</script>
+
+<template>
+  <div>
+    <h2>書籍一覧</h2>
+    <ul>
+      <li v-for="book in books" :key="book.id">
+        {{ book.title }} - {{ book.author }}
+      </li>
+    </ul>
+  </div>
+</template>
