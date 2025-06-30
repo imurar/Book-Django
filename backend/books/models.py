28f4b924fdda9253114e46aaa8a6a3827de2a272
imurar <imurar@outlook.com>
2025-06-30 19:58:10 +0900

    ステータス機能の作成

--- backend/books/models.py
+++ backend/books/models.py
@@ -1,8 +1,15 @@
 from django.db import models
 
+class Status(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
 class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=50)
+    status = models.ForeignKey(Status, on_delete=models.CASCADE, related_name='books')
 
     def __str__(self):
-        return self.title
+        return f"{self.title} ({self.status})"
