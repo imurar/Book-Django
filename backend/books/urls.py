1603c839d54a6091bfd49ac0915a4447e90a21d6
imurar <imurar@outlook.com>
2025-07-03 09:10:32 +0900

    urls.py に status API のルーティングを追加

--- backend/books/urls.py
+++ backend/books/urls.py
@@ -1,9 +1,10 @@
 from rest_framework.routers import DefaultRouter
-from .views import BookViewSet
+from .views import BookViewSet, StatusVieSet
 from django.urls import path, include
 
 router = DefaultRouter()
 router.register(r'books', BookViewSet)
+router.register(r'statuses', StatusViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
